e974ada82b14eb8ea474640cd0b87ec870abf271
Chris "Blue" Moore <cmoore@itrsgroup.com>
2019-08-23 15:50:41 -0500

    Fixed ur stuff again

--- testgo.go
+++ testgo.go
@@ -11,29 +11,35 @@ import (
 	"github.com/soniah/gosnmp"
 )
 
-func hostLoader() {
-	file, err := os.Open("hosts.txt")
+func hostLoader(path string) ([]string, error) {
+	file, err := os.Open(path)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	defer file.Close()
 
+	var lines []string
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
+	// scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
 
 	// Returns a boolean based on whether there's a next instance of `\n`
 	// character in the IO stream. This step also advances the internal pointer
 	// to the next position (after '\n') if it did find that token.
-	read := scanner.Scan()
+	// read := scanner.Scan()
 
-	if read {
-		fmt.Println("read byte array: ", scanner.Bytes())
-		fmt.Println("read bool: ", scanner.Text())
-	}
+	// if read {
+	// 	fmt.Println("read byte array: ", scanner.Bytes())
+	// 	fmt.Println("read bool: ", scanner.Text())
+	// }
 
-	return read
+	// return read
 }
 
 func main() {
@@ -54,11 +60,17 @@ func main() {
 	}
 
 	// Sets the value of the main() scope variable read to the return value of hostLoader
-	read = hostLoader()
+	read, read_err := hostLoader("hosts.txt")
+
+	if read_err != nil {
+		fmt.Printf("read error")
+	}
+
+	var target = ""
 
 	// For each item in the array read, as host, do w/e
 	for _, host := range read {
-		fmt.Printf(host)
+		target = host
 	}
 
 	var oid string = "1.3.6.1.2.1.31.1.1.1.18"
