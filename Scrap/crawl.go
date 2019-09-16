fb4aa6812f043862e152af9eb6df7efc21051846
Ken Dobbins <kdobbins@itrsgroup.com>
2019-09-16 17:46:30 -0400

    Adjusted formatting, added var declaration

---
+++ Scrap/crawl.go
@@ -0,0 +1,45 @@
+package Scrap
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"github.com/soniah/gosnmp"
+	"github.com/golang/mock/gomock"
+	"log"
+)
+
+func hosts() {
+	file, err := os.Open("hosts.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	// Returns a boolean based on whether there's a next instance of `\n`
+	// character in the IO stream. This step also advances the internal pointer
+	// to the next position (after '\n') if it did find that token.
+	read := scanner.Scan()
+
+	if read {
+		fmt.Println("read byte array: ", scanner.Bytes())
+		fmt.Println("read string: ", scanner.Text())
+	}
+}
+
+func walk(x *GoSNMP) { Walk(rootOid 1.3.6.1.2.1.31.1.1.1.18, walkFn WalkFunc) error 
+	return x.walk(GetNextRequest, rootOid, walkFn)
+}
+
+func snmpwalk() {
+	for _ := range hosts()
+}
+
+func main() {
+	hostparse = hosts.do()
+	walk = snmpwalk.do()
+}
---
+++ Scrap/snmpwalk.go
@@ -0,0 +1,67 @@
+package Scrap
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/soniah/gosnmp"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("Usage:\n")
+		fmt.Printf("   %s [-community=<community>] host [oid]\n", filepath.Base(os.Args[0]))
+		fmt.Printf("     host      - the host to walk/scan\n")
+		fmt.Printf("     oid       - the MIB/Oid defining a subtree of values\n\n")
+		flag.PrintDefaults()
+	}
+
+	var community string
+	flag.StringVar(&community, "community", "public", "the community string for device")
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	target := flag.Args()[0]
+	var oid string
+	if len(flag.Args()) > 1 {
+		oid = flag.Args()[1]
+	}
+
+	gosnmp.Default.Target = target
+	gosnmp.Default.Community = community
+	gosnmp.Default.Timeout = time.Duration(10 * time.Second) // Timeout better suited to walking
+	err := gosnmp.Default.Connect()
+	if err != nil {
+		fmt.Printf("Connect err: %v\n", err)
+		os.Exit(1)
+	}
+	defer gosnmp.Default.Conn.Close()
+
+	err = gosnmp.Default.BulkWalk(oid, printValue)
+	if err != nil {
+		fmt.Printf("Walk Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func printValue(pdu gosnmp.SnmpPDU) error {
+	var if_num = strings.Split(pdu.Name, ".")
+	fmt.Printf("IF_Num %s: ", if_num[len(if_num)-1])
+
+	switch pdu.Type {
+	case gosnmp.OctetString:
+		b := pdu.Value.([]byte)
+		fmt.Printf("STRING: %s\n", string(b))
+	default:
+		fmt.Printf("TYPE %d: %d\n", pdu.Type, gosnmp.ToBigInt(pdu.Value))
+	}
+	return nil
+}
